824c9ef2e2726771b761dc52af0746350d612f4f
Fabiola Rivera <fabiolarivera@ug.uchile.cl>
2019-04-27 09:35:57 -0400

    Modification of the class Energy Creation of the Trainer and Water Test

---
+++ test/TrainerTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrainerTest {
+
+    @Test
+    public void getActivePokemon() {
+    }
+
+    @Test
+    public void getHand() {
+    }
+
+    @Test
+    public void getTeam() {
+    }
+
+    @Test
+    public void setActivePokemon() {
+    }
+
+    @Test
+    public void setTeam() {
+    }
+
+    @Test
+    public void setHand() {
+    }
+
+    @Test
+    public void changeActivePokemon() {
+    }
+
+    @Test
+    public void attack() {
+    }
+}
---
+++ test/WaterTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WaterTest {
+
+    @Test
+    public void calcDamage() {
+    }
+
+    @Test
+    public void calcDamagePlant() {
+    }
+
+    @Test
+    public void calcDamageFire() {
+    }
+
+    @Test
+    public void calcDamageWater() {
+    }
+
+    @Test
+    public void calcDamageLightning() {
+    }
+
+    @Test
+    public void calcDamageFighting() {
+    }
+
+    @Test
+    public void calcDamagePsychic() {
+    }
+}
